1081f1dafa420bcd5a3d25788503e743b623f3d8
aristov <vv.aristov@gmail.com>
2016-07-27 21:27:54 +0300

    add fromXML method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fromDOM from './lib/fromDOM';
+import fromXML from './lib/fromXML';
 import toDOM from './lib/toDOM';
 
-const DON = { fromDOM, toDOM };
+const DON = { fromDOM, fromXML, toDOM };
 
 export default DON;

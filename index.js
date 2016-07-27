26b76b5590557639edb9918ee83efec39079f26d
aristov <vv.aristov@gmail.com>
2016-07-28 01:57:28 +0300

    jsdoc

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import fromDOM from './lib/fromDOM';
 import fromXML from './lib/fromXML';
 import toDOM from './lib/toDOM';
 
+/**
+ * @module DON
+ */
 const DON = { fromDOM, fromXML, toDOM };
 
 export default DON;
